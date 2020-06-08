24f095fa22d31d2e34bd6d1905ac2cf0392933de
Jens <jensdriller@gmail.com>
2020-06-08 12:07:42 -0700

    adding start date

--- first-dag.py
+++ first-dag.py
@@ -1,7 +1,14 @@
 from airflow import DAG
 from airflow.contrib.sensors.file_sensor import FileSensor
 
-with DAG(dag_id="first_dag", schedule_interval="*/20 * * * *" ) as dag:
+from datetime import datetime
+
+default_args = {
+    "start_date" : datetime(2020, 1, 1),
+    "owner": "airflow",
+}
+
+with DAG(dag_id="first_dag", schedule_interval="*/20 * * * *", default_args=default_args) as dag:
     waiting_for_file = FileSensor(
         task_id="waiting_for_file",
         fs_conn_id="fs_test",
